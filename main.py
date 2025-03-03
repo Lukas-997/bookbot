114ed66e7d556cf073d8cfd9c6e4c8e2121a62e5
Lukas-997 <luka.sajic@gmail.com>
2025-03-03 22:53:14 +0100

    finished project

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from stats import count_words, count_characters, sort_characters
+
+def get_book_text(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    
+    book_text = get_book_text(book_path)
+    num_words = count_words(book_text)
+    char_count = count_characters(book_text)
+    sorted_chars = sort_characters(char_count)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['count']}")
+    print("============= END ===============")
+
+if __name__ == "__main__":
+    main()
